6d60f7c1c91424c08fa3929e190f95e68601959b
Ryan Hasan <ryan.hasan@gmail.com>
2018-05-24 13:53:00 -0700

    Initial Commit

---
+++ gateway/gateway.h
@@ -0,0 +1,19 @@
+//
+//  gateway.h
+//  gateway
+//
+//  Created by Aminul Hasan on 5/24/18.
+//  Copyright © 2018 Evergent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for gateway.
+FOUNDATION_EXPORT double gatewayVersionNumber;
+
+//! Project version string for gateway.
+FOUNDATION_EXPORT const unsigned char gatewayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <gateway/PublicHeader.h>
+
+
